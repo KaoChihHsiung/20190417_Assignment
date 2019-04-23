e0aed27d660db7214ded06d050724e9172ce96cb
KaoChihHsiung <labkao@stm.org.tw>
2019-04-23 16:18:54 +0800

    5 update

--- Assigment_5.py
+++ Assigment_5.py
@@ -4,7 +4,7 @@ print('''作業5:給予一組學生成績 MatrixA=[100,20,84,96,78,60,55,99,90]�
 MatrixA = [100,20,84,96,78,60,55,99,90]     #定義學生成績為MatrixA列表List
 Len_Score = len(MatrixA)                    #計算MatrixA長度為Len_Score
 for i in range(0,Len_Score):                #用for迴圈產生(0~Len_Score長度)的數,List位置數
-    if MatrixA[i] >= 60:                    #判斷分數，若>=60，則輸出及格
-        print(i+1,'號',MatrixA[i],'及格')    #輸出號碼為i+1，...
-    else:                                   #判斷分數，若非>=60，則輸出不及格
-        print(i+1,'號',MatrixA[i],'不及格')
+    if MatrixA[i] >= 60:                    #判斷分數，若依索引值i取出學生成績>=60，則輸出及格
+        print(i+1,'號',MatrixA[i],'及格')    #輸出號碼為i+1，因為索引值從0開始，第一個數編號需加1
+    else:                                   #判斷if條件式的分數，若非>=60，則輸出不及格
+        print(i+1,'號',MatrixA[i],'不及格')  #輸出號碼為i+1，因為索引值從0開始，第一個數編號需加1
